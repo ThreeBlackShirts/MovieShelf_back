b63ca80aa1aa2236497a6c6294ff5075901f79b7
Jini-Eun <julietjin94@gmail.com>
2022-02-11 20:44:45 +0900

    First commit & project setting

---
+++ README.md
@@ -0,0 +1,3 @@
+# MovieShelf_backend
+
+
---
+++ src/main/java/com/blackshirts/movieshelf/MovieShelfApplication.java
@@ -0,0 +1,13 @@
+package com.blackshirts.movieshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieShelfApplication.class, args);
+    }
+
+}
