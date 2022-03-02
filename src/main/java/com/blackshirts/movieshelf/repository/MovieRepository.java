1bb094ee433fe598adc783a0b476bee8b0731919
SShinMJ <winny0678@gmail.com>
2022-03-02 23:20:31 +0900

    Add DB setting for Crawling

---
+++ src/main/java/com/blackshirts/movieshelf/repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.blackshirts.movieshelf.repository;
+
+import com.blackshirts.movieshelf.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    //기본기능(sava(), findOne(), findAll(), count(), delete())을 제외한
+    //조회 기능을 추가하고 싶으면 규칙에 맞는 메서드를 추가
+    // EX) Movie findBytitle(String search_word);
+}
