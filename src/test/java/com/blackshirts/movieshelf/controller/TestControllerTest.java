34e35007ceabb63c90185d8d7270e7fd1e58f027
Jini-Eun <julietjin94@gmail.com>
2022-03-13 02:48:53 +0900

    Add TestControllerTest

---
+++ src/test/java/com/blackshirts/movieshelf/controller/TestControllerTest.java
@@ -0,0 +1,28 @@
+package com.blackshirts.movieshelf.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
+class TestControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
+        mockMvc.perform(get("/api/get"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+}
