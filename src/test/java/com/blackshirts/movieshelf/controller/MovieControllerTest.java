1133d3cb9fa380ff1a26849c34e1ec94dfebf4ed
SShinMJ <winny0678@gmail.com>
2022-03-13 16:33:21 +0900

    Add MovieControllerTest and MovieResponseDtoTest

---
+++ src/test/java/com/blackshirts/movieshelf/controller/MovieControllerTest.java
@@ -0,0 +1,39 @@
+package com.blackshirts.movieshelf.controller;
+
+import com.blackshirts.movieshelf.service.MovieService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+//@WebMvcTest(MovieController.class)
+public class MovieControllerTest {
+
+    @Autowired //스프링이 관리하는 빈 주입을 받는다.
+    private MockMvc mockMvc; // 웹 API 테스트 시 사용(HTTP GET, POST), 스프링 MVC의 시작
+
+    @MockBean //특정 Bean 을 모의 객체로 주입받아 사용
+    private MovieService movieService;
+
+    @Test
+    public void movieSave() throws Exception {
+        mockMvc.perform(get("/movie/moviedatainsert"))
+                .andExpect(status().isOk()); //status검증
+    }
+
+    @Test
+    public void findAllMovies() throws Exception {
+        mockMvc.perform(get("/movie/movielist"))
+                .andExpect(status().isOk()) //status검증
+                .andDo(print());
+    }
+}
---
+++ src/test/java/com/blackshirts/movieshelf/dto/MovieResponseDtoTest.java
@@ -0,0 +1,34 @@
+package com.blackshirts.movieshelf.dto;
+
+import com.blackshirts.movieshelf.entity.Movie;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest //JPA 관련 테스트 설정만 로드
+class MovieResponseDtoTest {
+
+    @Test
+    public void lombok_function_test(){
+        //given
+        int movieRank = 1;
+        String movieTitle = "영화제목";
+        String moviePoster = "포스터URL";
+        String movieContentBold = "한줄 줄거리";
+        String movieContentDetail = "상세 줄거리";
+        String movieContentDetailLong = "상세 줄거리2";
+
+        //when
+        MovieResponseDto dto = new MovieResponseDto(movieRank, movieTitle, moviePoster, movieContentBold, movieContentDetail, movieContentDetailLong);
+
+        //then
+        assertThat(dto.getMovieRank()).isEqualTo(movieRank); //assertThat: 테스트 검증 라이브러리
+        assertThat(dto.getMovieTitle()).isEqualTo(movieTitle);
+        assertThat(dto.getMoviePoster()).isEqualTo(moviePoster);
+        assertThat(dto.getMovieContentBold()).isEqualTo(movieContentBold);
+        assertThat(dto.getMovieContentDetail()).isEqualTo(movieContentDetail);
+        assertThat(dto.getMovieContentDetailLong()).isEqualTo(movieContentDetailLong);
+    }
+
+}
