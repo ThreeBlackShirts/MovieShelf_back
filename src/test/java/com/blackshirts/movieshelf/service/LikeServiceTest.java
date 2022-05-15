af52f405a2e19e6f2516d06a6ae971c6b8ab8d1f
JiniEun <julietjin94@gmail.com>
2022-05-15 18:15:29 +0900

    Fix LikeServiceTest

--- src/test/java/com/blackshirts/movieshelf/service/LikeServiceTest.java
+++ src/test/java/com/blackshirts/movieshelf/service/LikeServiceTest.java
@@ -1,8 +1,20 @@
 package com.blackshirts.movieshelf.service;
 
+import com.blackshirts.movieshelf.repository.LikeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class LikeServiceTest {
+
+    @InjectMocks
+    private LikeService likeService;
+
+    @Mock
+    private LikeRepository likeRepository;
 
 
     @Test
