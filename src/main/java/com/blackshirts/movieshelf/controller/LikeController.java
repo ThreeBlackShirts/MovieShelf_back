b2f335266ee4ea49817d0871ede90c53206df398
JiniEun <julietjin94@gmail.com>
2022-05-15 19:14:39 +0900

    Fix LikeController

--- src/main/java/com/blackshirts/movieshelf/controller/LikeController.java
+++ src/main/java/com/blackshirts/movieshelf/controller/LikeController.java
@@ -1,6 +1,7 @@
 package com.blackshirts.movieshelf.controller;
 
 import com.blackshirts.movieshelf.dto.UserLoginRequestDto;
+import com.blackshirts.movieshelf.dto.UserRequestDto;
 import com.blackshirts.movieshelf.exception.BaseResponse;
 import com.blackshirts.movieshelf.exception.BaseResponseCode;
 import com.blackshirts.movieshelf.service.LikeService;
@@ -19,12 +20,12 @@ public class LikeController {
     private final LikeService likeService;
 
     @PostMapping("/like/{reviewId}")
-    public BaseResponse<String> addLike(UserLoginRequestDto user, @PathVariable Long reviewId) {
+    public BaseResponse<String> addLike(UserRequestDto userRequestDto, @PathVariable Long reviewId) {
 
         boolean result = false;
 
 
-        return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), likeService.addLike(user, reviewId));
+        return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), likeService.addLike(userRequestDto.toEntity(), reviewId));
 
     }
 
