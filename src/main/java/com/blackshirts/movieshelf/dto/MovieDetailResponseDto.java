a9d0eeb33a7fa381e14f23c3016e3c19479d987c
SShinMJ <winny0678@gmail.com>
2022-06-04 15:16:36 +0900

    Fix Movie Detail return data

---
+++ src/main/java/com/blackshirts/movieshelf/dto/MovieDetailResponseDto.java
@@ -0,0 +1,47 @@
+package com.blackshirts.movieshelf.dto;
+
+import com.blackshirts.movieshelf.entity.Movie;
+import com.blackshirts.movieshelf.entity.MovieStillcut;
+import com.blackshirts.movieshelf.entity.MovieTrailer;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MovieDetailResponseDto {
+    private int movieRank;
+    private String movieTitle;
+    private String moviePoster;
+    private String movieGenres;
+    private String movieNation;
+    private String movieRunningTime;
+    private String movieReleaseDate;
+    private String movieDirector;
+    private String movieActor;
+    private String movieFilmrate;
+    private String movieContentBold;
+    private String movieContentDetail;
+    private List<String> movieStillcut;
+    private List<String> movieTrailer;
+
+    public MovieDetailResponseDto(Movie movie, MovieStillcut movieStillcut, MovieTrailer movieTrailer){
+        this.movieRank = movie.getMovieRank();
+        this.movieTitle = movie.getMovieTitle();
+        this.moviePoster = movie.getMoviePoster();
+        this.movieGenres = movie.getMovieGenres();
+        this.movieNation = movie.getMovieNation();
+        this.movieRunningTime = movie.getMovieRunningTime();
+        this.movieReleaseDate = movie.getMovieReleaseDate();
+        this.movieDirector = movie.getMovieDirector();
+        this.movieActor = movie.getMovieActor();
+        this.movieFilmrate = movie.getMovieFilmrate();
+        this.movieContentBold = movie.getMovieContentBold();
+        this.movieContentDetail = movie.getMovieContentDetail();
+        this.movieStillcut.add(movieStillcut.getStillcut());
+        this.movieTrailer.add(movieTrailer.getTralier());
+    }
+}
--- src/main/java/com/blackshirts/movieshelf/repository/MovieTrailerRepository.java
+++ src/main/java/com/blackshirts/movieshelf/repository/MovieTrailerRepository.java
@@ -5,7 +5,9 @@ import com.blackshirts.movieshelf.entity.MovieTrailer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieTrailerRepository extends JpaRepository<MovieTrailer, Long> {
-
+    List<MovieTrailer> findByMovie(Movie movie);
 }
