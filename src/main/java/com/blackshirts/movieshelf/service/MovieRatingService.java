4ae8d9aa96c5f03b6546e059fde21e60c8eaf500
JiniEun <julietjin94@gmail.com>
2022-06-05 17:01:27 +0900

    Add isNotAlreadyRating function

--- src/main/java/com/blackshirts/movieshelf/service/MovieRatingService.java
+++ src/main/java/com/blackshirts/movieshelf/service/MovieRatingService.java
@@ -1,13 +1,24 @@
 package com.blackshirts.movieshelf.service;
 
+import com.blackshirts.movieshelf.entity.Movie;
+import com.blackshirts.movieshelf.entity.Review;
+import com.blackshirts.movieshelf.entity.User;
+import com.blackshirts.movieshelf.exception.BaseException;
+import com.blackshirts.movieshelf.exception.BaseResponseCode;
 import com.blackshirts.movieshelf.repository.MovieRatingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieRatingService {
     private final MovieRatingRepository movieRatingRepository;
+
+    //사용자가 이미 별점을 준 영화인지 확인
+    private boolean isNotAlreadyRating(User user, Movie movie) {
+        return movieRatingRepository.findByMovieAndUser(movie, user).isPresent();
+    }
 
 
 
