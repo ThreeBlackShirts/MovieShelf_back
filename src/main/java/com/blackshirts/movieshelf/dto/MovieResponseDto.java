b701e351d680490aaf515f6e23349a4f2c97176a
Jin <julietjin94@gmail.com>
2022-06-26 23:42:27 +0900

    Fix : Movie ResponseDto - add movieId

--- src/main/java/com/blackshirts/movieshelf/dto/MovieResponseDto.java
+++ src/main/java/com/blackshirts/movieshelf/dto/MovieResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class MovieResponseDto {
+    private Long movieId;
     private int movieRank;
     private String movieTitle;
     private String moviePoster;
@@ -19,6 +20,7 @@ public class MovieResponseDto {
     private String movieContentDetail;
 
     public MovieResponseDto(Movie movie){
+        this.movieId = movie.getMovieId();
         this.movieRank = movie.getMovieRank();
         this.movieTitle = movie.getMovieTitle();
         this.moviePoster = movie.getMoviePoster();
@@ -33,6 +35,19 @@ public class MovieResponseDto {
         this.movieContentDetail = movie.getMovieContentDetail();
     }
 
-    public MovieResponseDto(int movieRank, String movieTitle, String moviePoster, String movieGenres, String movieNation, String movieRunningTime, String movieReleaseDate, String movieDirector, String movieActor, String movieFilmrate, String movieContentBold, String movieContentDetail) {
+    public MovieResponseDto(Long movieId, int movieRank, String movieTitle, String moviePoster, String movieGenres, String movieNation, String movieRunningTime, String movieReleaseDate, String movieDirector, String movieActor, String movieFilmrate, String movieContentBold, String movieContentDetail) {
+        this.movieId = movieId;
+        this.movieRank = movieRank;
+        this.movieTitle = movieTitle;
+        this.moviePoster = moviePoster;
+        this.movieGenres = movieGenres;
+        this.movieNation = movieNation;
+        this.movieRunningTime = movieRunningTime;
+        this.movieReleaseDate = movieReleaseDate;
+        this.movieDirector = movieDirector;
+        this.movieActor = movieActor;
+        this.movieFilmrate = movieFilmrate;
+        this.movieContentBold = movieContentBold;
+        this.movieContentDetail = movieContentDetail;
     }
 }
--- src/main/java/com/blackshirts/movieshelf/dto/MovieSearchResponseDto.java
+++ src/main/java/com/blackshirts/movieshelf/dto/MovieSearchResponseDto.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class MovieSearchResponseDto {
+    private Long movieId;
     private String movieTitle;
     private String moviePoster;
 
     public MovieSearchResponseDto(Movie movie) {
+        this.movieId = movie.getMovieId();
         this.movieTitle = movie.getMovieTitle();
         this.moviePoster = movie.getMoviePoster();
     }
