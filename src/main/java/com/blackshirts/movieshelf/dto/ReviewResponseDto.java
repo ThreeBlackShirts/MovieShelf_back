c9f8927a9bab85e11b021e70bb4f9a1a80241701
SShinMJ <winny0678@gmail.com>
2022-07-09 18:52:37 +0900

    Fix Review Response

--- src/main/java/com/blackshirts/movieshelf/dto/ReviewResponseDto.java
+++ src/main/java/com/blackshirts/movieshelf/dto/ReviewResponseDto.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 @Getter
 public class ReviewResponseDto {
     private Long reviewId;
+    private String writer;
     private String user;
     private String title;
     private String content;
 
     public ReviewResponseDto(Review entity) {
         this.reviewId = entity.getReviewId();
+        this.writer = entity.getUser().getUserEmail();
         this.user = entity.getUser().getUserNickname();
         this.title = entity.getTitle();
         this.content = entity.getContent();
