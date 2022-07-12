76a7d907debd06f833659ae6debdfd73a0b29c7c
Jin <julietjin94@gmail.com>
2022-07-13 04:17:39 +0900

    fix : ReviewListResponseDto to get movie_title, movie_poster

--- src/main/java/com/blackshirts/movieshelf/dto/ReviewListResponseDto.java
+++ src/main/java/com/blackshirts/movieshelf/dto/ReviewListResponseDto.java
@@ -8,12 +8,16 @@ public class ReviewListResponseDto {
     private Long reviewId;
     private String user;
     private Long movieId;
+    private String movieTitle;
+    private String moviePoster;
     private String title;
 
     public ReviewListResponseDto(Review entity) {
         this.reviewId = entity.getReviewId();
         this.user = entity.getUser().getUserNickname();
         this.movieId = entity.getMovie().getMovieId();
+        this.movieTitle = entity.getMovie().getMovieTitle();
+        this.moviePoster = entity.getMovie().getMoviePoster();
         this.title = entity.getTitle();
     }
 }
