d341683f2e07311b979c6a7d337021315369204c
Jin <julietjin94@gmail.com>
2022-07-13 17:55:57 +0900

    fix : WishListResponseDto

--- src/main/java/com/blackshirts/movieshelf/dto/WishListResponseDto.java
+++ src/main/java/com/blackshirts/movieshelf/dto/WishListResponseDto.java
@@ -7,10 +7,14 @@ import lombok.Getter;
 @Getter
 public class WishListResponseDto {
     private Long movieId;
+    private String movieTitle;
+    private String moviePoster;
     private String userEmail;
 
     public WishListResponseDto(WishList entity) {
         this.userEmail = entity.getUser().getUserEmail();
         this.movieId = entity.getMovie().getMovieId();
+        this.movieTitle = entity.getMovie().getMovieTitle();
+        this.moviePoster = entity.getMovie().getMoviePoster();
     }
 }
