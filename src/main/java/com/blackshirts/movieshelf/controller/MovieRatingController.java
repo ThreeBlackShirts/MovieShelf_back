7fcd2078caae1ab95b74bd92286c7aeb7363b4e6
Jin <julietjin94@gmail.com>
2022-07-31 05:29:31 +0900

    feat : MovieRating Service, Controller add, update function

--- src/main/java/com/blackshirts/movieshelf/controller/MovieRatingController.java
+++ src/main/java/com/blackshirts/movieshelf/controller/MovieRatingController.java
@@ -13,23 +13,27 @@ import org.springframework.web.bind.annotation.*;
 @Api(tags = {"2-1. MovieRating"})
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v2/movierate")
+@RequestMapping("/api/v2/movie/rate")
 public class MovieRatingController {
 
     private final MovieRatingService movieRatingService;
 
     private static final Logger log = LoggerFactory.getLogger(MovieRatingController.class);
 
-    @PostMapping("/{movieId}")
-    public BaseResponse addMovieRate(@RequestBody MovieRatingRequestDto movieRatingRequestDto, @PathVariable Long reviewId) {
+    @PostMapping("/add")
+    public BaseResponse addMovieRate(@RequestBody MovieRatingRequestDto movieRatingRequestDto) {
         log.info("addMovieRate() "+movieRatingRequestDto.getMovieRate());
 
         return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), movieRatingService.create(movieRatingRequestDto));
 
     }
 
-//    @DeleteMapping("/{reviewId}")
-//    public BaseResponse deleteLike(@RequestBody UserRequestDto userRequestDto, @PathVariable Long reviewId) {
-//        return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), likeService.delete(userRequestDto.toEntity(), reviewId));
-//    }
+    @PostMapping("/update/{reviewId}")
+    public BaseResponse updateMovieRate(@RequestBody MovieRatingRequestDto movieRatingRequestDto, @PathVariable Long reviewId) {
+        log.info("updateMovieRate() "+movieRatingRequestDto.getMovieRate());
+
+        return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), movieRatingService.updateMovieRate(movieRatingRequestDto));
+
+    }
+
 }
--- src/main/java/com/blackshirts/movieshelf/util/MovieRank.java
+++ src/main/java/com/blackshirts/movieshelf/util/MovieRank.java
@@ -1,7 +1,11 @@
 package com.blackshirts.movieshelf.util;
 
+import com.blackshirts.movieshelf.service.MovieRatingService;
+
 // Movie rank 관련 함수 가질 클래스
 public class MovieRank {
+//    public double calculateRank()
+
 
 
 }
