0ea41a4c3e0e923c070abfc6964389c9e8893fc5
Jin <julietjin94@gmail.com>
2022-07-31 12:32:32 +0900

    fix : optimize classes

--- src/main/java/com/blackshirts/movieshelf/service/WishListService.java
+++ src/main/java/com/blackshirts/movieshelf/service/WishListService.java
@@ -1,10 +1,8 @@
 package com.blackshirts.movieshelf.service;
 
-import com.blackshirts.movieshelf.dto.LikeResponseDto;
 import com.blackshirts.movieshelf.dto.UserRequestDto;
 import com.blackshirts.movieshelf.dto.WishListResponseDto;
 import com.blackshirts.movieshelf.entity.Movie;
-import com.blackshirts.movieshelf.entity.Review;
 import com.blackshirts.movieshelf.entity.User;
 import com.blackshirts.movieshelf.entity.WishList;
 import com.blackshirts.movieshelf.exception.BaseException;
